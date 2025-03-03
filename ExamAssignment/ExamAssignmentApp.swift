b28d06edafb4e9b66e6ab2a4c45c9e763a8f2dbe
anhvo <anh.vo@watasoftware.com>
2025-03-03 15:39:32 +0700

    Initial Commit

---
+++ ExamAssignment/ExamAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExamAssignmentApp.swift
+//  ExamAssignment
+//
+//  Created by Anh Vo on 3/3/25.
+//
+
+import SwiftUI
+
+@main
+struct ExamAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ExamAssignmentTests/ExamAssignmentTests.swift
@@ -0,0 +1,16 @@
+//
+//  ExamAssignmentTests.swift
+//  ExamAssignmentTests
+//
+//  Created by Anh Vo on 3/3/25.
+//
+
+import Testing
+
+struct ExamAssignmentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
